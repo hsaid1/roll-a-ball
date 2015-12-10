d8e48c1c166fac9943db81fe869ee149d9f4cd62
hsaid1 <Heba@nbp-243-248.nbp.ruw.rutgers.edu>
2015-12-10 17:22:20 -0500

    rollaball
    version 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+	void start(){
+		count = 0;
+		setCountText();
+		winText.text = "";
+	}
+	
+	void FixedUpdate() 
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		
+		GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Pickup"){
+			other.gameObject.SetActive(false);
+			count++;
+			setCountText();
+		}
+	}
+	void setCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 10) {
+			winText.text = "YOU WIN!";
+		}
+	}
+
+}
